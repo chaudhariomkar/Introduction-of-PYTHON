ed47600c6122de3010f95bdb75252778ffd8d338
OMKAR KANTILAL CHAUDHARI <126695492+chaudhariomkar@users.noreply.github.com>
2023-03-01 21:29:27 +0530

    Add files via upload

---
+++ pythonintro.py
@@ -0,0 +1,68 @@
+""""""
+"""
+    Python is an 
+interpreted (means running code line by line), 
+object-oriented (means this language is based on objects (such as data) rather than functions), 
+high-level (means it's easy for humans to understand) programming language 
+with dynamic semantics (means its variable are dynamic object, and they exist at run time level)
+developed by GUIDO VAN ROSSUM in the year 1991
+    Python is generally used to create web and mobile software, in artificial intelligence(AI) and 
+machine learning(ML), to perform data manipulation and analysis, and much more.
+    Python is popular because.
+1.Python is open-source.
+2.Python is simple and easy to learn.
+3.Python is versatile.
+4.Python is used a lot.
+"""
+a = 100
+#here a(identifier) = 100(object/entity)
+#Identifier is the name given to the object, which will act as a reference. Using this name one can recognize an object
+"""
+Rules to delcare an Identifier
+1.a-z,A-Z are valid (Python is a case sensitive language a=!A)
+2._(underscroll) is allowed
+3.Space is not allowed in between
+4.Special symbols & characters not allowed
+5.a.Number as a prefix is not allowed
+  b.Number as a suffix is allowed
+6.Numbers are not a valid identifier
+7.Numbers in between is allowed
+8.Reserved keywords are not allowed
+   How to check Reserve Keywords ? 
+   -keyword.kwlist 
+--------------------------------------------------------
+Indentation in python
+    In python, indentation is used to define blocks of code such as functions,loops & control structures.
+The level of indentation determines the scope of the code block,and all statements within the same block must 
+have the same level of indentation. The recommended indentation level is 4 spaces, although tabs can also be used.
+Inconsistent or incorrect indentation can cause syntax errors in python.  
+--------------------------------------------------------
+Data types in Python
+It contains 14 data types
+  Numeric
+1.int (0-9)
+2.float (12.5 - floor.ceil)
+3.complex (1+x - real+img)
+  Boolean (if you're dealing with logical,comparison,identity,membership operators - Boolean Output)
+4.True
+5.False
+6.String ' '
+7.List [ ]
+8.Tuple ( )
+9.Set { }
+10.Dictionary { }
+11.Frozenset ( )
+12.Range ( )
+13.Bytes ( )
+14.Bytearray ( )
+   None
+---------------------------------------------
+Mutable & Immutable Datatypes in Python
+Mutable - Values can be changed after they are created
+1.List
+2.Dictionary
+3.Set
+4.Bytes array
+& Others are Immutable such as int,float,str,tuple
+Immutable - Values cannot be changed after they are created
+"""
